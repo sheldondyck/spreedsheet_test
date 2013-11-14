1acd339ec709923b48e68a3922720c5fcaf92e88
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-11-14 10:05:15 -0200

    playing

--- app/assets/javascripts/tinysheet.js
+++ app/assets/javascripts/tinysheet.js
@@ -1,6 +1,6 @@
-for (var i=0; i<6; i++) {
+for (var i=0; i<10; i++) {
     var row = document.querySelector("table").insertRow(-1);
-    for (var j=0; j<6; j++) {
+    for (var j=0; j<10; j++) {
         var letter = String.fromCharCode("A".charCodeAt(0)+j-1);
         row.insertCell(-1).innerHTML = i&&j ? "<input id='"+ letter+i +"'/>" : i||letter;
     }
